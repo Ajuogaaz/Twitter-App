31375537aae8de0c73e2e90bdadd42f4b66b58c3
Linus Brian Okoth <okoth@fb.com>
2020-06-28 17:49:28 -0400

    Imported Tweet Content into ComposeActivity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -8,16 +8,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
+
+
 public class ComposeActivity extends AppCompatActivity {
+
 
     public static  final int MAX_TWEET_LENGTH = 140;
     EditText etCompose;
     Button btnTweet;
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        client = TwitterApp.getRestClient(this);
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
@@ -28,21 +37,33 @@ public class ComposeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String s = etCompose.getText().toString();
+                String tweetContent = etCompose.getText().toString();
 
-                if(s.isEmpty()){
+                if(tweetContent.isEmpty()){
                     Toast.makeText(ComposeActivity.this,
                             R.string.noEmptyTweet,
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(s.length() > MAX_TWEET_LENGTH){
+                if(tweetContent.length() > MAX_TWEET_LENGTH){
                     Toast.makeText(ComposeActivity.this,
                             R.string.tweetTooLong,
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Toast.makeText(ComposeActivity.this, s, Toast.LENGTH_SHORT).show();
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_SHORT).show();
+
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+                    }
+                });
 
 
 
