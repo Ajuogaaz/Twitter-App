c14ad66720df282167614db5c0939b3cbfa23efa
Linus Brian Okoth <okoth@fb.com>
2020-06-29 12:17:12 -0400

    Created the class that defines the querry return model

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
@@ -3,9 +3,14 @@ package com.codepath.apps.restclienttemplate.models;
 import androidx.room.Dao;
 import androidx.room.Query;
 
+import java.util.List;
+
 @Dao
 public  interface TweetDao {
 
+    //this is a sample querry.
     @Query("SELECT * FROM Tweet ORDER BY createdAt DESC LIMIT 300")
     List<TweetWithUser> recentItems();
+
+
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetWithUser.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetWithUser.java
@@ -1,6 +1,19 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import androidx.room.Embedded;
+
 public class TweetWithUser {
+
+    /*
+    To solve the problem of not getting collective data from the foreign code.Thus we
+    do an embedding of the data
+    */
+
+    @Embedded
+    User user;
+
+    @Embedded(prefix = "tweet_")
+    Tweet tweet;
 
 
 }
