507bf2951b79649cdcc2048ab1c3b6e57c056995
Linus Brian Okoth <okoth@fb.com>
2020-06-29 09:50:04 -0400

    Added primary key to user and other column infp

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,6 +1,8 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,15 +12,26 @@ import org.parceler.Parcel;
 @Entity
 public class User {
 
+    @ColumnInfo
+    @PrimaryKey
+    public long id;
+
+    @ColumnInfo
     public String name;
+
+    @ColumnInfo
     public String screenName;
+
+    @ColumnInfo
     public String profileImageUrl;
+
 
     public User(){};
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
 
         User user = new User();
+        user.id = jsonObject.getLong("id");
         user.name = jsonObject.getString("name");
         user.screenName = jsonObject.getString("screen_name");
         user.profileImageUrl = jsonObject.getString("profile_image_url_https");
