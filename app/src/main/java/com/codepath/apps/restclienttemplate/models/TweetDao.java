6d7db6295a108a57672764eeb57199f51d66fb20
Linus Brian Okoth <okoth@fb.com>
2020-06-29 12:27:11 -0400

    Configured the call for the prefix

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
@@ -8,8 +8,9 @@ import java.util.List;
 @Dao
 public  interface TweetDao {
 
-    //this is a sample querry.
-    @Query("SELECT * FROM Tweet ORDER BY createdAt DESC LIMIT 300")
+    //Combine the tweets with User factoring in the fact that you prepended some data
+    @Query("SELECT Tweet.body AS tweet_body, Tweet.createdAt AS tweet_createdAt, Tweet.Id AS tweet_id " +
+            "FROM Tweet INNER JOIN User ON  Tweet.userId = User.id ORDER BY createdAt DESC LIMIT 5")
     List<TweetWithUser> recentItems();
 
 
