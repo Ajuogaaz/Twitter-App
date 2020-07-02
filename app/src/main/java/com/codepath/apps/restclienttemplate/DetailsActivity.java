dce5b1901b2083208061dffa4f6e0d9162138f79
Linus Brian Okoth <okoth@fb.com>
2020-07-02 03:18:11 -0400

    Innitialized binding for the DetailsActivity class

--- app/src/main/java/com/codepath/apps/restclienttemplate/DetailsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/DetailsActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.codepath.apps.restclienttemplate.databinding.ActivityDetailsBinding;
+import com.codepath.apps.restclienttemplate.databinding.ActivityTimelineBinding;
+
 public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        final ActivityDetailsBinding binding = ActivityDetailsBinding.inflate(getLayoutInflater());
 
+        View view = binding.getRoot();
 
-
-
-        setContentView(R.layout.activity_details);
+        setContentView(view);
 
 
     }
