1adf46afaed84a2de09c4e14c51141f96e18cea0
Linus Brian Okoth <okoth@fb.com>
2020-07-02 03:19:36 -0400

    Unwrapped the intent from the Timeline Class

--- app/src/main/java/com/codepath/apps/restclienttemplate/DetailsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/DetailsActivity.java
@@ -7,8 +7,13 @@ import android.view.View;
 
 import com.codepath.apps.restclienttemplate.databinding.ActivityDetailsBinding;
 import com.codepath.apps.restclienttemplate.databinding.ActivityTimelineBinding;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.parceler.Parcels;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    Tweet tweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,8 @@ public class DetailsActivity extends AppCompatActivity {
         View view = binding.getRoot();
 
         setContentView(view);
+
+        tweet = Parcels.unwrap(getIntent().getParcelableExtra(Tweet.class.getSimpleName()));
 
 
     }
